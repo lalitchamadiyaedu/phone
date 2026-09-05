899d3959c727e3ce8c74b75c3ddac1a830cc154d
Lalit Chamadiya <lalit.chamadiya@ngivbt.edu.in>
2026-09-05 23:21:44 +0530

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "Installing pip and dependencies..."
-python3 -m ensurepip --upgrade || true
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+echo "Installing dependencies..."
+if command -v uv &> /dev/null; then
+    uv pip install --system -r requirements.txt
+else
+    python3 -m pip install --break-system-packages -r requirements.txt
+fi
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
