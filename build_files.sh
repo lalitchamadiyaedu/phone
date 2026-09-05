5270ad339124e2a9017175550752f416d9098607
Lalit Chamadiya <lalit.chamadiya@ngivbt.edu.in>
2026-09-05 23:01:11 +0530

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+echo "Installing pip and dependencies..."
+python3 -m ensurepip --upgrade || true
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
